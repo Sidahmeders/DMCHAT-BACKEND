3b34a5f62ed3bd60faf8a82faa76ffa4e2d76378
= <=>
2023-05-22 12:38:02 +0100

    add fetchAllPatients controller

--- src/controllers/patientController.js
+++ src/controllers/patientController.js
@@ -1,12 +1,12 @@
 const { Patient } = require('../models')
 
-// @description     Create New Patient
-// @route           POST /api/patient
+// @description     fetch all Patients
+// @route           GET /api/patient
 // @access          Protected
-const createPatient = async (req, res) => {
+const fetchAllPatients = async (req, res) => {
   try {
-    const patient = await Patient.create(req.body)
-    res.status(200).json(patient)
+    const patients = await Patient.find()
+    res.status(200).json(patients)
   } catch (error) {
     return res.status(400).json({
       success: false,
@@ -33,7 +33,24 @@ const fetchPatientsByName = async (req, res) => {
   }
 }
 
+// @description     Create New Patient
+// @route           POST /api/patient
+// @access          Protected
+const createPatient = async (req, res) => {
+  try {
+    const patient = await Patient.create(req.body)
+    res.status(200).json(patient)
+  } catch (error) {
+    return res.status(400).json({
+      success: false,
+      statusCode: 400,
+      message: error.message,
+    })
+  }
+}
+
 module.exports = {
   createPatient,
+  fetchAllPatients,
   fetchPatientsByName,
 }
--- src/routes/patientRoutes.js
+++ src/routes/patientRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const { protect } = require('../middleware')
-const { createPatient, fetchPatientsByName } = require('../controllers/patientController')
+const { fetchAllPatients, fetchPatientsByName, createPatient } = require('../controllers/patientController')
 
 const router = express.Router()
 
+router.route('/').get(protect, fetchAllPatients)
 router.route('/:name').get(protect, fetchPatientsByName)
 
 router.route('/').post(protect, createPatient)
