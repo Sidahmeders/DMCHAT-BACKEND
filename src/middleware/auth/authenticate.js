496505abf4011a6c0e10ee8e1660cc7c5b7bb3f6
Ahmed Boutaraa <ahorob57@gmail.com>
2023-07-09 01:32:22 +0100

    refactor authenticate middleware

--- src/middleware/auth/authenticate.js
+++ src/middleware/auth/authenticate.js
@@ -13,23 +13,28 @@ const authenticate = async (req, res, next) => {
       }
 
       const { id: userId } = verifyToken(token)
-      req.user = await User.findById(userId).select('-password')
+      const foundUser = await User.findById(userId).select('-password')
+
+      if (!foundUser) {
+        return res.status(401).json({
+          error: { message: 'User not found!' },
+        })
+      }
+
+      req.user = foundUser
 
       next()
     } catch (error) {
       if (error.name === 'MongooseError') {
-        return res.status(503).json({
-          error: {
-            message: `Mongoose error: ${error.message}`,
-          },
-        })
-      } else {
-        return res.status(401).json({
-          error: {
-            message: 'Not authorized, token failed',
-          },
-        })
+        return res.status(503).json({ error: { message: `Mongoose error: ${error.message}` } })
       }
+      if (error.name === 'TokenExpiredError') {
+        return res.status(401).json({ error: { message: 'Token Expired!' } })
+      }
+      if (error.name === 'JsonWebTokenError') {
+        return res.status(401).json({ error: { message: 'No Token was found or Invalid Signature!' } })
+      }
+      res.status(500).json({ error: { message: error.message } })
     }
   }
 }
